ad830a08d07358b9ab98fab3cf6789ea597e5128
javed khalil <admin@techmynd.com>
2019-11-24 18:30:46 +0500

    update

--- template-to-start-with/components/navigation/Navigator.js
+++ template-to-start-with/components/navigation/Navigator.js
@@ -3,8 +3,14 @@ import { Platform } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 
+import {createAppContainer} from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
-createStackNavigator({
-  
+const MainNavigator = createStackNavigator({
+  Home: {screen: HomeScreen},
+  Profile: {screen: ProfileScreen},
 });
+
+const App = createAppContainer(MainNavigator);
+
+export default AppNavigator;
